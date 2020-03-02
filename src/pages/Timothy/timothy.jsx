ef94e14be2461a38d9179954b264b3acfcbdbbd7
TimGCY <timothygoh@snsoft.my>
2020-03-02 16:45:37 +0800

    timothy site

--- src/pages/Timothy/timothy.jsx
+++ src/pages/Timothy/timothy.jsx
@@ -3,29 +3,46 @@
 */
 import React from "react";
 import BasePage from "../basePage/basePage";
+import "./timothy.scss";
 
 export default class LandingPage extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //     };
-  //   }
-
   render() {
     const timothy = {
       name: "Timothy Goh",
-      gender: "male",
       age: 22,
       occupation: "developer",
-      hobby: "photography"
+      hobby: "photography",
+      birthday: "6th May 1998",
+      email: "timothygoh@snsoft.my",
+      contact: "+60 16 - 576 9856"
     };
-    const { name, gender, age, occupation, hobby } = timothy;
+    const { name, age, occupation, hobby, birthday, email, contact } = timothy;
     return (
-      <BasePage title={"Timothy Goh"}>
-        Hi, I am {name}.<br />A {gender} obviously. I'm a 90's and now I'm {age}
-        , working as a {occupation}.
-        <br />
-        I'm a {hobby} lover.
+      <BasePage>
+        <div className={"container box"}>
+          <div className={"card-layout"}>
+            <div className={"media img-panel"}>
+              <img
+                src="https://pbs.twimg.com/profile_images/1150311201668157441/XMMLZugM_400x400.png"
+                alt="profile-pic"
+              />
+            </div>
+            <div className={"desc-panel"}>
+              <h6>Hi, I am</h6>
+              <h3>{name}</h3>
+              <h4>Junior Developer</h4>
+              <p>
+                I'm a 90's and now I'm {age}, workingas a {occupation}.<br />
+                I'm a {hobby} lover.
+              </p>
+              <ul className={"basic-info"}>
+                <li>{birthday}</li>
+                <li>{contact}</li>
+                <li>{email}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </BasePage>
     );
   }
