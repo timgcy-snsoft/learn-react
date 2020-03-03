6afbf13651f5ad790ee1769cab52f3f9285d7046
YC-Liew <61528112+YC-Liew@users.noreply.github.com>
2020-03-03 08:55:32 +0800

    Try Update

--- src/pages/YC_Liew/PortFolio.jsx
+++ src/pages/YC_Liew/PortFolio.jsx
@@ -24,6 +24,7 @@ export default class LandingPage extends React.Component {
                         <div>Contact Number : 017-2112912</div>
                         <div>Strength Coding : C# , Java</div>
                     </div>
+                    <div className="About">About Me</div>
                     <p className="paragrah">
                         I am study in TARUC (Tunku Abdul Rahman College) bachelor degree in Software Developer. I have learn before C# , Java , SQL , html & css and soon. I am a student who are enjoy to design a web and coding. My hobby is swimming.
                     </p>
