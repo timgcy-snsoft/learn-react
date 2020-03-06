fbdfa475d204872ecc2f5a12ecac566e16ebf7a8
ChanVinShenSNSoft <vschan@snsoft.my>
2020-03-06 10:59:39 +0800

    no message

--- src/pages/DotaAPI/FetchFunction.jsx
+++ src/pages/DotaAPI/FetchFunction.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 const API_HOST = `https://api.opendota.com/api`;
 
 const callApi = async endpoint => {
   let response = await fetch(`${API_HOST}${endpoint}`);
   let data = await response.json();
   return data;
-=======
-const getAPI = async arg => {
-    let response = await fetch(arg);
-    let data = await response.json();
-    return data;
->>>>>>> 5a849211450b0cb4aafbe5984deb50950de55a19
 };
 
 export default callApi;
