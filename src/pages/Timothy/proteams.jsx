1ac106e73c706d2efecd211c4c34f549746f7ccc
Timothy Goh <timothygoh@snsoft.my>
2020-03-31 08:57:13 +0800

    using callAPI function

--- src/pages/Timothy/proteams.jsx
+++ src/pages/Timothy/proteams.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import "./proteam.scss";
-import { Link } from "react-router-dom";
+import React from 'react'
+import './proteam.scss'
+import { Link } from 'react-router-dom'
+import callApi from '../DotaAPI/FetchFunction'
 
 const teamIknow = [
-  "OG",
-  "TNC Predator",
-  "Vici Gaming",
-  "Team Secret",
-  "Invictus Gaming",
-  "Fnatic"
-];
+  'OG',
+  'TNC Predator',
+  'Vici Gaming',
+  'Team Secret',
+  'Invictus Gaming',
+  'Fnatic',
+]
 export default class proTeam extends React.Component {
   /**
    * constructor
@@ -18,12 +19,12 @@ export default class proTeam extends React.Component {
    * @object  @state  component state
    */
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      items: [],
-      isLoaded: false
-    };
+      teams: [],
+      isLoaded: false,
+    }
   }
 
   /**
@@ -31,26 +32,11 @@ export default class proTeam extends React.Component {
    *
    * Fetch json array of objects from given url and update state.
    */
-  componentDidMount() {
-    var teamArr = [];
-    fetch("https://api.opendota.com/api/teams")
-      .then(res => res.json())
-      .then(json => {
-        for (const a in json) {
-          for (const arr in teamIknow) {
-            if (json[a].name.toLowerCase() === teamIknow[arr].toLowerCase()) {
-              teamArr.push(json[a]);
-            }
-          }
-        }
-        this.setState({
-          items: teamArr,
-          isLoaded: true
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  async componentDidMount() {
+    this.setState({
+      teams: await callApi('/teams'),
+      isLoaded: true,
+    })
   }
 
   /**
@@ -59,9 +45,17 @@ export default class proTeam extends React.Component {
    * Render UI
    */
   render() {
-    const { isLoaded, items } = this.state;
+    const { isLoaded, teams } = this.state
+    let filteredTeam = []
+    for (const i in teams) {
+      for (const j in teamIknow) {
+        if (teams[i].name.toLowerCase() === teamIknow[j].toLowerCase()) {
+          filteredTeam.push(teams[i])
+        }
+      }
+    }
 
-    if (!isLoaded) return <div>Loading...</div>;
+    if (!isLoaded) return <div>Loading...</div>
 
     return (
       <div className="slideshow">
@@ -69,18 +63,18 @@ export default class proTeam extends React.Component {
         <div className="slideshow-container">
           <ul>
             <div className="slideshow-slide">
-              {items.map(item => (
-                <Link to={`/team/${item.team_id}/players`}>
+              {filteredTeam.map(team => (
+                <Link to={`/team/${team.team_id}/players`}>
                   <div className="teamProfile">
-                    <li key={item.team_id}>
+                    <li key={team.team_id}>
                       <div className="slideshow-img">
-                        <img src={item.logo_url} alt="" />
+                        <img src={team.logo_url} alt="" />
                       </div>
                       <div className="teamDetail">
-                        <div className="teamName">{item.name}</div>
+                        <div className="teamName">{team.name}</div>
                         <div className="result-container">
-                          <div className="result win">Win: {item.wins}</div>
-                          <div className="result lose">Lose: {item.losses}</div>
+                          <div className="result win">Win: {team.wins}</div>
+                          <div className="result lose">Lose: {team.losses}</div>
                         </div>
                       </div>
                     </li>
@@ -91,6 +85,6 @@ export default class proTeam extends React.Component {
           </ul>
         </div>
       </div>
-    );
+    )
   }
 }
--- src/pages/Timothy/teamPlayer.jsx
+++ src/pages/Timothy/teamPlayer.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import BasePage from "../basePage/basePage";
-import "./teamPlayer.scss";
-import { Link } from "react-router-dom";
+import React from 'react'
+import BasePage from '../basePage/basePage'
+import './teamPlayer.scss'
+import { Link } from 'react-router-dom'
+import callApi from '../DotaAPI/FetchFunction'
 
 export default class TeamPlayer extends React.Component {
   /**
@@ -11,13 +12,13 @@ export default class TeamPlayer extends React.Component {
    * @object  @state  component state
    */
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       teamId: this.props.match.params.id,
       team: [],
-      match: false
-    };
+      match: false,
+    }
   }
 
   /**
@@ -25,19 +26,11 @@ export default class TeamPlayer extends React.Component {
    *
    * Fetch json array of objects from given url and update state.
    */
-  componentDidMount() {
-    fetch(`https://api.opendota.com/api/teams/${this.state.teamId}/players`)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          team: data,
-          isLoaded: true
-        });
-        // console.log(data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  async componentDidMount() {
+    this.setState({
+      team: await callApi(`/teams/${this.state.teamId}/players`),
+      isLoaded: true,
+    })
   }
 
   /**
@@ -46,16 +39,16 @@ export default class TeamPlayer extends React.Component {
    * Render UI
    */
   render() {
-    const { id, isLoaded, team } = this.state;
+    const { id, isLoaded, team } = this.state
     const imgUrl =
-      "https://true-education.org/wp-content/uploads/2015/10/facebook-profile-picture-unknown-facts-about-facebook.jpg";
+      'https://true-education.org/wp-content/uploads/2015/10/facebook-profile-picture-unknown-facts-about-facebook.jpg'
 
     const winRate = (wins, games) => {
-      var rate = (parseInt(wins) / parseInt(games)) * 100;
-      return rate.toFixed(2);
-    };
+      var rate = (parseInt(wins) / parseInt(games)) * 100
+      return rate.toFixed(2)
+    }
 
-    if (!isLoaded) return <div>Loading...</div>;
+    if (!isLoaded) return <div>Loading...</div>
 
     return (
       <div className="team">
@@ -73,7 +66,7 @@ export default class TeamPlayer extends React.Component {
 
                       <figcaption className="card__caption">
                         <h3 className="card__name">
-                          {player.name ? player.name : "no name"}
+                          {player.name ? player.name : 'no name'}
                         </h3>
 
                         <table className="card__stats">
@@ -105,6 +98,6 @@ export default class TeamPlayer extends React.Component {
           </ul>
         </div>
       </div>
-    );
+    )
   }
 }
