f4cbd9de18fe799a409e376cadaec99e975dfb7b
Timothy Goh <timothygoh@snsoft.my>
2020-04-02 17:02:10 +0800

    nothing for now

--- src/pages/ChokKahYang/MatchAPI.jsx
+++ src/pages/ChokKahYang/MatchAPI.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react'
 import Popup from 'reactjs-popup'
 import MatchList from './MatchList'
 import Pagination from './Pagination'
-import callApi from "../DotaAPI/FetchFunction"
+import callApi from '../DotaAPI/FetchFunction'
 
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css"
-import subDays from "date-fns/subDays";
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import subDays from 'date-fns/subDays'
 
 import Modal from './Model'
 import ItemChart from './ItemChart'
@@ -17,104 +17,119 @@ const date = new Date()
 date.setDate(date.getDate() - 1)
 
 function MatchAPI({ publicMatches, heros, clusters, itemsID, items }) {
+  const [startDate, setStartDate] = useState(date)
+  const [endDate, setEndDate] = useState(new Date())
 
+  const [regions, setRegions] = useState({})
 
-    const [startDate, setStartDate] = useState(date)
-    const [endDate, setEndDate] = useState(new Date())
+  const [currentPage, setCurrentPage] = useState(1)
+  const [matchesPerPage] = useState(15)
 
-    const [regions, setRegions] = useState({})
+  const indexOfLastMatch = currentPage * matchesPerPage
+  const indexOfFirstMatch = indexOfLastMatch - matchesPerPage
+  const currentMatch = publicMatches.slice(indexOfFirstMatch, indexOfLastMatch)
 
-    const [currentPage, setCurrentPage] = useState(1)
-    const [matchesPerPage] = useState(15)
+  const paginate = pageNumber => setCurrentPage(pageNumber)
 
-    const indexOfLastMatch = currentPage * matchesPerPage
-    const indexOfFirstMatch = indexOfLastMatch - matchesPerPage
-    const currentMatch = publicMatches.slice(indexOfFirstMatch, indexOfLastMatch)
+  useEffect(() => {
+    const fetchRegions = async () => {
+      const region = await callApi('/constants/region')
+      setRegions({
+        ...region,
+        5: 'SE Asia',
+        8: 'RUSSIA',
+        12: 'CHINA',
+        13: 'CHINA',
+        17: 'CHINA',
+        18: 'CHINA',
+        20: 'CHINA',
+        25: 'CHINA',
+      })
+    }
+    fetchRegions()
+  }, [])
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+  return (
+    <div className="m-3 clearfix">
+      <div className="flex font-weight-bold text-light text-center">
+        <div className="m-2 w-25">Match ID</div>
+        <div className="m-2 w-25">Start Time</div>
+        <div className="m-2 w-25">Duration</div>
+        <div className="m-2 w-25">Result</div>
+        <div className="m-2 w-50">Radiant</div>
+        <div className="m-2 w-50">Dire</div>
+      </div>
+      {currentMatch.map(match => (
+        <MatchList
+          key={match.match_id}
+          match={match}
+          heros={heros}
+          clusters={clusters}
+          regions={regions}
+          itemsID={itemsID}
+          items={items}
+        />
+      ))}
 
-    useEffect(() => {
-        const fetchRegions = async () => {
-            const region = await callApi('/constants/region')
-            setRegions({
-                ...region,
-                5: "SE Asia", 8: "RUSSIA", 12: "CHINA", 13: "CHINA",
-                17: "CHINA", 18: "CHINA", 20: "CHINA", 25: "CHINA"
-            })
-        }
-        fetchRegions()
-    }, []);
+      <nav className="p-4 mr-4 float-left">
+        <ul className="pagination ">
+          <Popup
+            modal
+            trigger={<button className="btn btn-dark m-1">Pie Chart</button>}
+          >
+            {close => <Pie info={currentMatch} heros={heros} close={close} />}
+          </Popup>
 
-    return (
+          <Popup
+            modal
+            trigger={<button className="btn btn-dark m-1">Bar Chart</button>}
+          >
+            {close => <Modal close={close} />}
+          </Popup>
 
-        <div className="m-3 clearfix">
-            <div className="flex font-weight-bold text-light text-center">
-                <div className="m-2 w-25">Match ID</div>
-                <div className="m-2 w-25">Start Time</div>
-                <div className="m-2 w-25">Duration</div>
-                <div className="m-2 w-25">Result</div>
-                <div className="m-2 w-50">Radiant</div>
-                <div className="m-2 w-50">Dire</div>
-            </div>
-            {currentMatch.map(match =>
-                (<MatchList key={match.match_id}
-                    match={match} heros={heros} clusters={clusters} regions={regions}
-                    itemsID={itemsID} items={items} />)
-            )}
+          <Popup
+            modal
+            trigger={
+              <button className="btn btn-dark m-1">Statistic (Line)</button>
+            }
+          >
+            {close => <LineChart publicMatches={publicMatches} close={close} />}
+          </Popup>
 
-            <nav className="p-4 mr-4 float-left">
-                <ul className="pagination ">
-                    <Popup modal trigger={
-                        <button className="btn btn-dark m-1">
-                            Pie Chart
-                        </button>}>
-                        {close => <Pie info={currentMatch} heros={heros} close={close} />}
-                    </Popup>
+          <Popup
+            modal
+            trigger={<button className="btn btn-dark m-1">Items</button>}
+          >
+            <ItemChart items={items} startDate={startDate} endDate={endDate} />
+          </Popup>
+          <li>
+            <DatePicker
+              className="m-1"
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+              minDate={subDays(new Date(), 14)}
+              maxDate={new Date()}
+            />
+          </li>
+          <li>
+            <DatePicker
+              className="m-1"
+              selected={endDate}
+              onChange={date => setEndDate(date)}
+              minDate={subDays(new Date(), 13)}
+              maxDate={new Date()}
+            />
+          </li>
+        </ul>
+      </nav>
 
-                    <Popup modal trigger={
-                        <button className="btn btn-dark m-1">
-                            Bar Chart
-                        </button>}>
-                        {close => <Modal close={close} />}
-                    </Popup>
-
-                    <Popup modal trigger={
-                        <button className="btn btn-dark m-1">
-                            Statistic (Line)
-                        </button>}>
-                        {close => <LineChart close={close} />}
-                    </Popup>
-
-                    <Popup modal trigger={
-                        <button className="btn btn-dark m-1">
-                            Items
-                        </button>}>
-                        <ItemChart items={items} startDate={startDate} endDate={endDate} />
-                    </Popup>
-                    <li>
-                        <DatePicker className="m-1"
-                            selected={startDate}
-                            onChange={date => setStartDate(date)}
-                            minDate={subDays(new Date(), 14)}
-                            maxDate={new Date()} />
-                    </li>
-                    <li>
-                        <DatePicker className="m-1"
-                            selected={endDate}
-                            onChange={date => setEndDate(date)}
-                            minDate={subDays(new Date(), 13)}
-                            maxDate={new Date()} />
-                    </li>
-                </ul>
-
-            </nav>
-
-
-            <Pagination matchesPerPage={matchesPerPage} totalMatches={publicMatches.length} paginate={paginate} />
-        </div>
-    )
-
-
+      <Pagination
+        matchesPerPage={matchesPerPage}
+        totalMatches={publicMatches.length}
+        paginate={paginate}
+      />
+    </div>
+  )
 }
 
 export default MatchAPI
--- src/pages/Timothy/lineChart.jsx
+++ src/pages/Timothy/lineChart.jsx
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
-import { Line } from 'react-chartjs-2'
+import { Line, defaults } from 'react-chartjs-2'
 import callAPI from '../DotaAPI/FetchFunction'
+
+// defaults.global.animation = false
 
 class LineChart extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      matchData: [],
+      matchData: this.props.publicMatches,
       lineData: {
         labels: ['0-10', '11-20', '21-30', '31-40', '41-50', '51-60', '> 60'],
         datasets: [
           {
             // label: 'Games completed within the time',
             fill: false,
+            lineTension: 0,
             backgroundColor: 'rgba(75,192,192,0.4)',
             borderColor: 'rgba(75,192,192,1)',
-            lineTension: 0,
-            data: [],
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
           },
         ],
       },
@@ -26,7 +40,6 @@ class LineChart extends Component {
 
   async componentDidMount() {
     this.setState({
-      matchData: await callAPI('/publicMatches'),
       isLoaded: true,
     })
     this.matchTimeCount()
@@ -40,48 +53,55 @@ class LineChart extends Component {
       gp5 = 0,
       gp6 = 0,
       gp7 = 0
-    const matches = this.state.matchData
-    for (const i in matches) {
-      if (Math.floor(matches[i].duration / 60) > 60) gp7++
-      else if (
-        Math.floor(matches[i].duration / 60) > 50 &&
-        Math.floor(matches[i].duration) >= 60
-      )
-        gp6++
-      else if (
-        Math.floor(matches[i].duration / 60) > 40 &&
-        Math.floor(matches[i].duration) >= 50
-      )
-        gp5++
-      else if (
-        Math.floor(matches[i].duration / 60) > 30 &&
-        Math.floor(matches[i].duration) >= 40
-      )
-        gp4++
-      else if (
-        Math.floor(matches[i].duration / 60) > 20 &&
-        Math.floor(matches[i].duration) >= 30
-      )
-        gp3++
-      else if (
-        Math.floor(matches[i].duration / 60) > 10 &&
-        Math.floor(matches[i].duration) >= 20
-      )
-        gp2++
-      else gp1++
-    }
-    this.setState({
-      lineData: {
-        datasets: [
-          {
-            data: [gp1, gp2, gp3, gp4, gp5, gp6, gp7],
-          },
-        ],
-      },
-    })
+    const matches = this.props.publicMatches
+    console.table(matches)
+    // for (const i in matches) {
+    //   if (Math.floor(matches[i].duration / 60) > 60) gp7++
+    //   else if (
+    //     Math.floor(matches[i].duration / 60) > 50 &&
+    //     Math.floor(matches[i].duration) >= 60
+    //   )
+    //     gp6++
+    //   else if (
+    //     Math.floor(matches[i].duration / 60) > 40 &&
+    //     Math.floor(matches[i].duration) >= 50
+    //   )
+    //     gp5++
+    //   else if (
+    //     Math.floor(matches[i].duration / 60) > 30 &&
+    //     Math.floor(matches[i].duration) >= 40
+    //   )
+    //     gp4++
+    //   else if (
+    //     Math.floor(matches[i].duration / 60) > 20 &&
+    //     Math.floor(matches[i].duration) >= 30
+    //   )
+    //     gp3++
+    //   else if (
+    //     Math.floor(matches[i].duration / 60) > 10 &&
+    //     Math.floor(matches[i].duration) >= 20
+    //   )
+    //     gp2++
+    //   else gp1++
+    // }
+    // this.setState({
+    //   lineData: {
+    //     datasets: [
+    //       {
+    //         data: [gp1, gp2, gp3, gp4, gp5, gp6, gp7],
+    //       },
+    //     ],
+    //   },
+    // })
   }
 
   render() {
+    if (!this.state.isLoaded)
+      return (
+        <div style={{ alignItem: 'center', justifyContent: 'center' }}>
+          Loading...
+        </div>
+      )
     return (
       <div>
         <h4 style={{ margin: 'auto' }}>
